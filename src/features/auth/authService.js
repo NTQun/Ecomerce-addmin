269bbe525ea7fff279aad1433421f3f21052e963
quan <nguyenquan07112001@gmail.com>
2023-02-18 13:11:15 +0700

    product list api

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { base_url } from "../../utils/base_url";
+import { base_url } from "../../utils/baseUrl";
 
 const login = async (userData) => {
   const response = await axios.post(`${base_url}user/admin-login`, userData);
