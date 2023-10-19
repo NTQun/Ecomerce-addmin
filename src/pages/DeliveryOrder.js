39b0496a913733f1f78fef3e2567e9f061d62470
Quan <github004@cvidpro.net>
2023-10-19 11:31:48 +0700

    update search table in page list

--- src/pages/DeliveryOrder.js
+++ src/pages/DeliveryOrder.js
@@ -1,52 +1,12 @@
-import React, { useEffect } from "react";
-import { Table } from "antd";
+import React, { useEffect, useRef, useState } from "react";
+import { Button, Input, Space, Table } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import {
   getAllOrderDeliver,
   updateOrderDeliver,
 } from "./../features/delivery/deliverySlice";
-
-const columns = [
-  {
-    title: "SNo",
-    dataIndex: "key",
-  },
-  {
-    title: "Name",
-    dataIndex: "name",
-  },
-  {
-    title: "Detail Order",
-    dataIndex: "detailorder",
-  },
-  {
-    title: "Collect money",
-    dataIndex: "amount",
-  },
-  {
-    title: "Date",
-    dataIndex: "date",
-  },
-  {
-    title: " Address",
-    dataIndex: "address",
-  },
-  {
-    title: "Sub Address",
-    dataIndex: "subaddress",
-  },
-
-  {
-    title: "Mobile",
-    dataIndex: "mobile",
-  },
-
-  {
-    title: "Actions",
-    dataIndex: "action",
-  },
-];
+import { SearchOutlined } from "@ant-design/icons";
 
 const DeliveryOrder = () => {
   const dispatch = useDispatch();
@@ -103,6 +63,166 @@ const DeliveryOrder = () => {
       ),
     });
   }
+
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
+  const searchInput = useRef(null);
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText("");
+  };
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+      close,
+    }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: "block",
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+          {/* <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button> */}
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? "#1677ff" : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    // render: (text) =>
+    //   searchedColumn === dataIndex ? (
+    //     <Highlighter
+    //       highlightStyle={{
+    //         backgroundColor: "#ffc069",
+    //         padding: 0,
+    //       }}
+    //       searchWords={[searchText]}
+    //       autoEscape
+    //       textToHighlight={text ? text.toString() : ""}
+    //     />
+    //   ) : (
+    //     text
+    //   ),
+  });
+
+  const columns = [
+    {
+      title: "SNo",
+      dataIndex: "key",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      ...getColumnSearchProps("name"),
+    },
+    {
+      title: "Detail Order",
+      dataIndex: "detailorder",
+    },
+    {
+      title: "Collect money",
+      dataIndex: "amount",
+    },
+    {
+      title: "Date",
+      dataIndex: "date",
+    },
+    {
+      title: " Address",
+      dataIndex: "address",
+      ...getColumnSearchProps("address"),
+    },
+    {
+      title: "Sub Address",
+      dataIndex: "subaddress",
+      ...getColumnSearchProps("subaddress"),
+    },
+
+    {
+      title: "Mobile",
+      dataIndex: "mobile",
+      ...getColumnSearchProps("mobile"),
+    },
+
+    {
+      title: "Actions",
+      dataIndex: "action",
+    },
+  ];
 
   return (
     <div>
--- src/pages/Statistics.js
+++ src/pages/Statistics.js
@@ -1,47 +1,8 @@
-import React, { useEffect } from "react";
-import { Table } from "antd";
+import React, { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getOrders } from "../features/auth/authSlice";
-
-const columns = [
-  {
-    title: "SNo",
-    dataIndex: "key",
-  },
-  {
-    title: "Product",
-    dataIndex: "product",
-    sorter: (a, b) => a.product.length - b.product.length,
-  },
-  {
-    title: "Product Img",
-    dataIndex: "img",
-  },
-  {
-    title: "Quantity",
-    dataIndex: "quantity",
-    sorter: (a, b) => a.quantity - b.quantity,
-  },
-  {
-    title: "Price",
-    dataIndex: "price",
-    sorter: (a, b) => a.price - b.price,
-  },
-  {
-    title: "Amount",
-    dataIndex: "amount",
-    sorter: (a, b) => a.amount - b.amount,
-  },
-  {
-    title: "Date",
-    dataIndex: "date",
-  },
-  {
-    title: " Statistics",
-    dataIndex: "statistics",
-    sorter: (a, b) => a.statistics - b.statistics,
-  },
-];
+import { SearchOutlined } from "@ant-design/icons";
+import { Button, Input, Space, Table } from "antd";
 
 const Statistic = () => {
   const dispatch = useDispatch();
@@ -80,6 +41,162 @@ const Statistic = () => {
     }
   }
   const total = data1.reduce((pre, curr) => pre + curr.statistics, 0);
+
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
+  const searchInput = useRef(null);
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText("");
+  };
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+      close,
+    }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: "block",
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+          {/* <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button> */}
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? "#1677ff" : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    // render: (text) =>
+    //   searchedColumn === dataIndex ? (
+    //     <Highlighter
+    //       highlightStyle={{
+    //         backgroundColor: "#ffc069",
+    //         padding: 0,
+    //       }}
+    //       searchWords={[searchText]}
+    //       autoEscape
+    //       textToHighlight={text ? text.toString() : ""}
+    //     />
+    //   ) : (
+    //     text
+    //   ),
+  });
+  const columns = [
+    {
+      title: "SNo",
+      dataIndex: "key",
+    },
+    {
+      title: "Product",
+      dataIndex: "product",
+      sorter: (a, b) => a.product.length - b.product.length,
+      ...getColumnSearchProps("product"),
+    },
+    {
+      title: "Product Img",
+      dataIndex: "img",
+    },
+    {
+      title: "Quantity",
+      dataIndex: "quantity",
+      sorter: (a, b) => a.quantity - b.quantity,
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+      sorter: (a, b) => a.price - b.price,
+    },
+    {
+      title: "Amount",
+      dataIndex: "amount",
+      sorter: (a, b) => a.amount - b.amount,
+    },
+    {
+      title: "Date",
+      dataIndex: "date",
+    },
+    {
+      title: " Statistics",
+      dataIndex: "statistics",
+      sorter: (a, b) => a.statistics - b.statistics,
+    },
+  ];
+
   return (
     <div>
       <h3 className="mb-4">Orders</h3>
